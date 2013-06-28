e2d77fd4f2f0e4ad4e48610e4f2ea07b32e7469a
Meelap Shah <meelapshah@gmail.com>
2013-06-28 21:53:03 +0000

    start bugfixes

--- kronos/storage/backends/memory/client.py
+++ kronos/storage/backends/memory/client.py
@@ -64,10 +64,9 @@ class InMemoryStorage(BaseStorage):
         self.db[stream].pop(0)
       bisect.insort(self.db[stream], Event(event))
     
-  def _retrieve(self, stream, start_id, end_time, max_items, configuration):
+  def _retrieve(self, stream, start_id, end_time, configuration):
     """
-    Return at most `max_items` events associated with `stream`, starting
-    with `start_id` and occuring before `end_time`.
+    Yield events from stream starting with `start_id` until `end_time`.
     """
 
     events_returned = 0
@@ -79,5 +78,3 @@ class InMemoryStorage(BaseStorage):
         break
       yield event
       events_returned += 1
-      if events_returned == max_items:
-        break
