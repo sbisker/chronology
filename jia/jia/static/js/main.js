5ec0523e7d26138d537855152c11089a837148d5
Meelap Shah <meelapshah@gmail.com>
2013-06-28 00:34:51 +0000

    save progress

--- jia/jia/static/js/main.js
+++ jia/jia/static/js/main.js
@@ -56,19 +56,48 @@ var VisView = Backbone.View.extend({
         var template = this.viewTypeToTemplate["plot"];
         this.$el.html(template(this.model.attributes));
 
+        var data = kronos_to_rickshaw(this.model.get("data"));
+        var series = new Array();
+        _.each(data, function(points, name) {
+            series.push({
+                name: name,
+                data: points,
+                color: "blue", // TODO(meelap) get random color
+            });
+        });
+
+        var element = this.$(".plot")[0];
+
         var graph = new Rickshaw.Graph({
-            element : this.$(".plot")[0],
+            element : element,
             width   : 400,
             height  : 250,
-            series  : this.model.get("data"),
+            series  : series,
             renderer: "line",
             min     : "auto",
         });
         graph.render();
+
+        var legend = new Rickshaw.Graph.Legend({
+            graph : graph,
+            element : element,
+        });
+
+        // TODO(meelap) need jquery ui sortable for this
+        //var shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
+            //graph : graph,
+            //legend : legend,
+        //});
+
+        var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight({
+            graph : graph,
+            legend : legend,
+        });
     },
 
     render_table : function() {
         var template = this.viewTypeToTemplate["table"];
+        this.$el.html(template(this.model.attributes));
     },
 
     render_new : function() {
@@ -97,6 +126,28 @@ var JiaView = Backbone.View.extend({
     },
 });
 
+function kronos_to_rickshaw(kronos) {
+    var rickshaw = {};
+    _.each(kronos, function(datapoint) {
+        _.each(datapoint, function(attrs, time) {
+            if (typeof(attrs) == "object") {
+                _.each(attrs, function(value, key) {
+                    if (!_.has(rickshaw, key)) {
+                        rickshaw[key] = new Array();
+                    }
+                    var t = parseInt(Date.parse(time).toString("yyyyMMdd"));
+                    rickshaw[key].push({x: t, y: value});
+                });
+            } else {
+                console.log("kronos_to_rickshaw: expecting dictionary ("+time+","+attrs+")");
+            }
+        });
+    });
+
+    return rickshaw;
+}
+
+f = kronos_to_rickshaw;
 
 // Don't use `var` so that it is accessible from console for debugging
 Visualizations = new VisCollection;
@@ -105,11 +156,23 @@ testdata = [
     { data : [ {x:0,y:0}, {x:1,y:1}, {x:2,y:4},{x:3,y:9}, {x:4,y:16},{x:5,y:25} ],
       name : "Test Data",
       color: "blue"}]
+// if typeof(value) == "object":
+//      do stacked plot
+//         totals column in table
+// else do regular plot/table
+referrer_signups = [
+    { "2013-01-01" : { "godaddy" : 10, "opentable" : 20 } },
+    { "2013-01-02" : { "godaddy" : 11, "opentable" : 22 } },
+    { "2013-01-03" : { "godaddy" : 12, "opentable" : 26 } },
+    { "2013-01-04" : { "godaddy" : 14, "opentable" : 22 } },
+    { "2013-01-05" : { "godaddy" : 15, "opentable" : 23 } }];
 
 testnew = new VisModel({type: "new"});
-testplot = new VisModel({type: "plot", data: testdata});
+testplot = new VisModel({type: "plot", data: referrer_signups});
+testtable = new VisModel({type: "table", data: referrer_signups});
 
 Visualizations.add(testplot);
+Visualizations.add(testtable);
 Visualizations.add(testnew);
 
 });
