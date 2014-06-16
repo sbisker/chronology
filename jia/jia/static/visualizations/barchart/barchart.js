57092a9b467766d82e47d20d90211850cb221ba4
Derek Schultz <derek@derekschultz.net>
2014-06-16 23:03:53 +0000

    Add safety checks for duplicate or misaligned labels

--- jia/jia/static/visualizations/barchart/barchart.js
+++ jia/jia/static/visualizations/barchart/barchart.js
@@ -30,37 +30,64 @@ module.factory('barchart', function () {
 
     this.setData = function (data, msg) {
       this.data = data;
+      var error = false;
+
+      var groups = [];
       var series = _.groupBy(data.events, function(event) {
         return event['@group'] || '';
       });
 
-      var cats = [];
+      var categories = [];
       if (_.size(series) > 0) {
+        var i = 0;
         series = _.map(series, function(events, seriesName) {
-          var i = 0;
-          return {name: seriesName, data: _.map(events, function(event) {
-            if (i == 0) {
-              cats.push(event['@label']);
+          var cats = [];
+
+          // Extract the values from the events and build a list
+          var data = _.map(events, function(event) {
+            // Build a list of categories (x axis) and check for duplicates
+            if (i == 0 && _.contains(cats, event['@label'])) {
+              msg.warn('Duplicate label "' + event['@label'] + '"');
             }
+            cats.push(event['@label']);
             return event['@value'];
-          })};
+          });
+          
+          // On first iteration, save the categories list
+          if (i == 0) {
+            categories = cats;
+            groups.push(event['@group']);
+          }
+          // On all other iterations, check to make sure the categories
+          // are consistent
+          else {
+            console.log(categories, cats);
+            if (!_.isEqual(categories, cats)) {
+              msg.error("All groups must have the same labels");
+              error = true;
+              return [];
+            }
+          }
+
           i++;
+
+          // C3 expects the group name to be the first item in the array
+          // followed by all the data points
+          data.unshift(events[0]['@group']);
+          return data;
         });
       } else {
         series = [];
         msg.warn("Data contains no events");
       }
-      this.series = series;
-      
-      var cols = [];
-      var groups = [];
-      _.each(series, function (group) {
-        var points = [group.name];
-        points = points.concat(group.data);
-        cols.push(points);
-        groups.push(group.name);
-      });
 
+      var cols = [];
+
+      // If there is an error, do not display the half calculated data
+      if(!error) {
+        cols = series;
+      }
+      
       this.c3data = {
         columns: cols,
         type: 'bar',
@@ -70,7 +97,7 @@ module.factory('barchart', function () {
       this.c3axis = {
         x: {
           type: 'category',
-          categories: cats
+          categories: categories
         }
       }
     }
